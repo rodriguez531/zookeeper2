2a0fde40b03b1e7791145b1e5c1bc779ece393b7
rodriguez531 <alexr5408@gmail.com>
2023-07-21 20:32:45 -0400

    added heroku port again

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
